fc4dbe5598f8f2285cd8e66678bb4ff64a49dbc0
viking <wiktor.przadka.1@gmail.com>
2021-01-07 09:21:57 +0100

    refactor way of organizing tests

--- efficiency-tests/main.py
+++ efficiency-tests/main.py
@@ -7,16 +7,16 @@ import time
 
 class RethinkTest:
 
-    def __init__(self):
+    def __init__(self, collection: str):
         r.connect('localhost', 28015).repl()
         try:
-            r.db('test').table_drop('inserts').run()
+            r.db('test').table_drop(collection).run()
         except ReqlRuntimeError:
             pass
-        r.db('test').table_create('inserts').run()
+        r.db('test').table_create(collection).run()
 
     @classmethod
-    def test_efficiency(cls, inserts_num: int):
+    def test_inserts_efficiency(cls, inserts_num: int):
         idx = 0
         print('test started')
 
@@ -28,18 +28,31 @@ class RethinkTest:
         print('test finished')
 
     @classmethod
-    def get_results(cls) -> list:
-        return list(r.table('inserts').run())
+    def test_read_write_efficiency(cls, inserts_num: int):
+        idx = 0
+        print('test started')
+
+        start = time.time()
+        while idx < inserts_num:
+            r.table('read_write').insert({'id': idx, 'time': time.time() - start}).run()
+            r.table('read_write').run()
+            idx += 1
+
+        print('test finished')
+
+    @classmethod
+    def get_results(cls, schema) -> list:
+        return list(r.table(schema).run())
 
 
 class MongoTest:
 
-    def __init__(self):
+    def __init__(self, collection: str):
         mongo_cli = MongoClient('127.0.0.1', 27017)
         self.db = mongo_cli.test
-        self.db.inserts.drop()
+        self.db[collection].drop()
 
-    def test_efficiency(self, inserts_num: int):
+    def test_inserts_efficiency(self, inserts_num: int):
         idx = 0
         print('test started')
 
@@ -50,11 +63,23 @@ class MongoTest:
 
         print('test finished')
 
-    def get_results(self) -> list:
-        return list(self.db.inserts.find())
+    def test_read_write_efficiency(self, inserts_num: int):
+        idx = 0
+        print('test started')
+
+        start = time.time()
+        while idx < inserts_num:
+            self.db.read_write.insert_one({'id': idx, 'time': time.time() - start})
+            self.db.read_write.find()
+            idx += 1
+
+        print('test finished')
+
+    def get_results(self, schema: str) -> list:
+        return list(self.db[schema].find())
 
 
-def plot_result(rethink_data: list, mongo_data: list):
+def plot_result(rethink_data: list, mongo_data: list, test_name: str):
     rethink_time_series = [row['time'] for row in sorted(rethink_data, key=lambda x: x['id'])]
     mongo_time_series = [row['time'] for row in sorted(mongo_data, key=lambda x: x['id'])]
 
@@ -68,20 +93,22 @@ def plot_result(rethink_data: list, mongo_data: list):
     plt.xlabel('number of inserted rows')
     plt.ylabel('time (s)')
 
-    plt.savefig('inserts.png')
+    plt.savefig(f'{test_name}.png')
 
 
 if __name__ == '__main__':
 
-    inserts_number = 10_000
+    inserts_number = 1_000
+    test_name = 'read_write'
 
-    rethink_test = RethinkTest()
-    rethink_test.test_efficiency(inserts_num=inserts_number)
+    rethink_test = RethinkTest(test_name)
+    rethink_test.test_read_write_efficiency(inserts_num=inserts_number)
 
-    mongo_test = MongoTest()
-    mongo_test.test_efficiency(inserts_num=inserts_number)
+    mongo_test = MongoTest(test_name)
+    mongo_test.test_read_write_efficiency(inserts_num=inserts_number)
 
     plot_result(
-        rethink_data=rethink_test.get_results(),
-        mongo_data=mongo_test.get_results()
+        rethink_data=rethink_test.get_results(test_name),
+        mongo_data=mongo_test.get_results(test_name),
+        test_name=test_name
     )
