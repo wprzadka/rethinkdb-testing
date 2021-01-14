4ad43055bd50213973b04f538b9aa6be313845c7
viking <wiktor.przadka.1@gmail.com>
2021-01-14 23:57:56 +0100

    compare rethink with couchDb

---
+++ efficiency-tests/couchdb_test.py
@@ -0,0 +1,42 @@
+import time
+import couchdb
+
+
+class CouchTest:
+    def __init__(self, schema: str):
+        user = "admin"
+        password = "password"
+        self.couchserver = couchdb.Server("http://%s:%s@127.0.0.1:5984/" % (user, password))
+
+        if schema in self.couchserver:
+            del self.couchserver[schema]
+        self.db = self.couchserver.create(schema)
+
+        select_all = 'function(doc) { emit((doc.id), doc); }'
+
+    def test_inserts_efficiency(self, inserts_num: int):
+        idx = 0
+        print('test started')
+
+        start = time.time()
+        while idx < inserts_num:
+            self.db.save({'id': idx, 'time': time.time() - start})
+            idx += 1
+
+        print('test finished')
+
+    def test_read_write_efficiency(self, inserts_num: int):
+        idx = 0
+        print('test started')
+
+        start = time.time()
+        while idx < inserts_num:
+            self.db.save({'id': idx, 'time': time.time() - start})
+            for i in self.db:
+                self.db.get(i)
+            idx += 1
+        print('test finished')
+
+    def get_results(self, schema: str) -> list:
+        db = self.couchserver[schema]
+        return list(db.get(i) for i in db)
--- efficiency-tests/main.py
+++ efficiency-tests/main.py
@@ -1,19 +1,27 @@
 from rethinkdb_test import RethinkTest
 from mongodb_test import MongoTest
+from couchdb_test import CouchTest
 import matplotlib.pyplot as plt
 
 
-def plot_result(rethink_data: list, mongo_data: list, test_name: str):
+def plot_result(
+        rethink_data: list,
+        mongo_data: list,
+        couch_data: list,
+        test_name: str
+):
     rethink_time_series = [row['time'] for row in sorted(rethink_data, key=lambda x: x['id'])]
     mongo_time_series = [row['time'] for row in sorted(mongo_data, key=lambda x: x['id'])]
-
+    couch_time_series = [row['time'] for row in sorted(couch_data, key=lambda x: x['id'])]
     print(rethink_time_series)
     print(mongo_time_series)
+    print(couch_time_series)
 
     plt.plot(rethink_time_series)
     plt.plot(mongo_time_series)
+    plt.plot(couch_time_series)
 
-    plt.legend(['RethinkDb', 'MongoDb'])
+    plt.legend(['RethinkDb', 'MongoDb', 'CouchDb'])
     plt.xlabel('number of iterations')
     plt.ylabel('time (s)')
 
@@ -23,7 +31,7 @@ def plot_result(rethink_data: list, mongo_data: list, test_name: str):
 
 if __name__ == '__main__':
 
-    iterations_num = 1_000
+    iterations_num = 5_00
     tests = ['inserts', 'read_write']
 
     for test_name in tests:
@@ -36,8 +44,13 @@ if __name__ == '__main__':
         mongo_test = MongoTest(test_name)
         getattr(mongo_test, f'test_{test_name}_efficiency')(inserts_num=iterations_num)
 
+        print("Couchdb")
+        couch_test = CouchTest(test_name)
+        getattr(couch_test, f'test_{test_name}_efficiency')(inserts_num=iterations_num)
+
         plot_result(
             rethink_data=rethink_test.get_results(test_name),
             mongo_data=mongo_test.get_results(test_name),
+            couch_data=couch_test.get_results(test_name),
             test_name=test_name
         )
