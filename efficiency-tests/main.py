80b464299f1d76df50618a23dbdcd0586c612200
viking <wiktor.przadka.1@gmail.com>
2021-01-15 08:57:42 +0100

    run test case only on proper classes

--- efficiency-tests/main.py
+++ efficiency-tests/main.py
@@ -5,27 +5,31 @@ import matplotlib.pyplot as plt
 
 
 def plot_result(
-        rethink_data: list,
-        mongo_data: list,
-        couch_data: list,
-        test_name: str
+        data_series_li: list,
+        series_names_li: list,
+        test_case_name: str
 ):
-    rethink_time_series = [row['time'] for row in sorted(rethink_data, key=lambda x: x['id'])]
-    mongo_time_series = [row['time'] for row in sorted(mongo_data, key=lambda x: x['id'])]
-    couch_time_series = [row['time'] for row in sorted(couch_data, key=lambda x: x['id'])]
-    print(rethink_time_series)
-    print(mongo_time_series)
-    print(couch_time_series)
+    time_series = [
+        [row['time'] for row in sorted(series, key=lambda x: x['id'])]
+        for series in data_series_li
+    ]
 
-    plt.plot(rethink_time_series)
-    plt.plot(mongo_time_series)
-    plt.plot(couch_time_series)
+    # mongo_time_series = [row['time'] for row in sorted(mongo_data, key=lambda x: x['id'])]
+    # couch_time_series = [row['time'] for row in sorted(couch_data, key=lambda x: x['id'])]
+    # print(rethink_time_series)
+    # print(mongo_time_series)
+    # print(couch_time_series)
+    for name, series in zip(series_names_li, time_series):
+        print(f'f{name}: {series}')
 
-    plt.legend(['RethinkDb', 'MongoDb', 'CouchDb'])
+    for series in time_series:
+        plt.plot(series)
+
+    plt.legend(series_names_li)
     plt.xlabel('number of iterations')
     plt.ylabel('time (s)')
 
-    plt.savefig(f'{test_name}.png')
+    plt.savefig(f'./plots/{test_case_name}.png')
     plt.close()
 
 
@@ -35,22 +39,39 @@ if __name__ == '__main__':
     tests = ['inserts', 'read_write']
 
     for test_name in tests:
+        test_func_name = f'test_{test_name}_efficiency'
         print(f'TEST: {test_name}')
-        print("RethinkDb")
-        rethink_test = RethinkTest(test_name)
-        getattr(rethink_test, f'test_{test_name}_efficiency')(iterations_num)
 
-        print("MongoDb")
-        mongo_test = MongoTest(test_name)
-        getattr(mongo_test, f'test_{test_name}_efficiency')(inserts_num=iterations_num)
+        series_names = []
+        data_series = []
 
-        print("Couchdb")
-        couch_test = CouchTest(test_name)
-        getattr(couch_test, f'test_{test_name}_efficiency')(inserts_num=iterations_num)
+        if hasattr(RethinkTest, test_func_name):
+            print('RethinkDb')
+            rethink_test = RethinkTest(test_name)
+            getattr(rethink_test, test_func_name)(iterations_num)
+
+            data_series.append(rethink_test.get_results(test_name))
+            series_names.append('RethinkDb')
+
+        if hasattr(MongoTest, test_func_name):
+            print('MongoDb')
+            mongo_test = MongoTest(test_name)
+            getattr(mongo_test, test_func_name)(iterations_num)
+
+            data_series.append(mongo_test.get_results(test_name))
+            series_names.append('MongoDb')
+
+        if hasattr(MongoTest, test_func_name):
+            print('Couchdb')
+            couch_test = CouchTest(test_name)
+            getattr(couch_test, test_func_name)(iterations_num)
+
+            data_series.append(couch_test.get_results(test_name))
+            series_names.append('Couchdb')
+
 
         plot_result(
-            rethink_data=rethink_test.get_results(test_name),
-            mongo_data=mongo_test.get_results(test_name),
-            couch_data=couch_test.get_results(test_name),
-            test_name=test_name
+            data_series_li=data_series,
+            series_names_li=series_names,
+            test_case_name=test_name
         )
