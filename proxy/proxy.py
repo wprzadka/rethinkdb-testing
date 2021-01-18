afe934fb75683f152a72422726c50c397a9e43ee
viking <wiktor.przadka.1@gmail.com>
2021-01-18 22:41:50 +0100

    save client message

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
         s.bind(('127.0.0.1', 28016))
         s.listen()
         conn, addr = s.accept()
+        # Connected by('127.0.0.1', 54854)
+        # b'\xc3\xbd\xc24{"protocol_version":0,"authentication_method":"SCRAM-SHA-256","authentication":"n,,n=admin,r=xylvqR28RzZY70ErNe7OPc6P"}\x00'
 
         with conn:
             print('Connected by', addr)
