d2f302bb9d665bd42a75040510afd681d68a9eb1
viking <wiktor.przadka.1@gmail.com>
2021-01-20 08:23:36 +0100

    check possible responses

--- proxy/injection/inject.py
+++ proxy/injection/inject.py
@@ -17,7 +17,7 @@ prepared_messages = [
     b'\x00\x00\x00\x00\x00\x00\x00\x00\x1b\x00\x00\x00[1,[62,[[14,["test"]]]],{}]',
     b'\x01\x00\x00\x00\x00\x00\x00\x00O\x00\x00\x00[1,[56,[[15,["proxy_test"]],{"source":"injector","time":1611125615.1374214}]],{}]',
     b'\x02\x00\x00\x00\x00\x00\x00\x00\x1a\x00\x00\x00[1,[15,["proxy_test"]],{}]',
-    b'\x02\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00[2]',
+    b'\x02\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00[2]'
 ]
 
 if __name__ == '__main__':
@@ -34,8 +34,29 @@ if __name__ == '__main__':
 """
 Server responses
 
-/home/viking/ComputerScience/software_testing/rethinkdb-testing/venv/bin/python /home/viking/ComputerScience/software_testing/rethinkdb-testing/proxy/injection/inject.py
 b'{"max_protocol_version":0,"min_protocol_version":0,"server_version":"2.3.2-windows-beta-584-gffe554","success":true}\x00'
 b'{"authentication":"r=hwE9dR7VXM3SYzl/IncHqRGUNdm6G6kuZKALxIdud9U9,s=/alRLo0bmsVf9VXrfVlGMA==,i=4096","success":true}\x00'
 b'{"error":"Wrong password","error_code":12,"success":false}\x00'
+"""
+"""
+b'{"max_protocol_version":0,"min_protocol_version":0,"server_version":"2.3.2-windows-beta-584-gffe554","success":true}\x00'
+b'{"authentication":"r=hwE9dR7VXM3SYzl/IncHqRGUaFLDYhxd1BQ9k53Mjs94,s=/alRLo0bmsVf9VXrfVlGMA==,i=4096","success":true}\x00'
+b'{"error":"Wrong password","error_code":12,"success":false}\x00'
+"""
+
+
+"""
+Server responses for auth with wrong name
+
+b'{"max_protocol_version":0,"min_protocol_version":0,"server_version":"2.3.2-windows-beta-584-gffe554","success":true}\x00'
+b'{"authentication":"r=hwE9dR7VXM3SYzl/IncHqRGUMvGUClYXHVqQl1MezZKM,s=C3QEaJmaadJe9sSwxAaMKg==,i=4096","success":true}\x00'
+b'{"error":"Unknown user","error_code":17,"success":false}\x00'
+"""
+
+"""
+Server response for modified "c" field [{"authentication":"c=bqws,..]
+
+b'{"max_protocol_version":0,"min_protocol_version":0,"server_version":"2.3.2-windows-beta-584-gffe554","success":true}\x00'
+b'{"authentication":"r=hwE9dR7VXM3SYzl/IncHqRGUUDnnbqL0WEZFC7wzfa7Z,s=/alRLo0bmsVf9VXrfVlGMA==,i=4096","success":true}\x00'
+b'{"error":"Invalid encoding","error_code":10,"success":false}\x00'
 """
