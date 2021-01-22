ce9e678da3498e9581a2d48c21ccbc49fdb7015f
wprzadka <wiktor.przadka.1@gmail.com>
2021-01-22 11:07:26 +0100

    add mongo join tables and sorting tests

--- efficiency-tests/mongodb_test.py
+++ efficiency-tests/mongodb_test.py
@@ -1,5 +1,6 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING
 import time
+import random
 
 
 class MongoTest:
@@ -30,6 +31,65 @@ class MongoTest:
             self.db.read_write.insert_one({'id': idx, 'time': time.time() - start})
             self.db.read_write.find()
             idx += 1
+
+        print('test finished')
+
+    def test_sorting_efficiency(self, inserts_num: int):
+        idx = 0
+        mod = inserts_num / min(100, inserts_num)
+
+        self.db.source.drop()
+
+        print('test started')
+
+        while idx < inserts_num:
+            self.db.source.insert_one({'value': idx})
+            if idx % mod == 0:
+                start = time.time()
+                for row in self.db.source.find().sort('value', ASCENDING):
+                    print(row, end=', ')
+                end = time.time()
+                print()
+                self.db.sorting.insert_one({'id': idx, 'time': end - start})
+            idx += 1
+
+        print('test finished')
+
+    def test_join_table_efficiency(self, inserts_num: int):
+
+        self.db.join_table_first.drop()
+        self.db.join_table_second.drop()
+
+        print('test started')
+
+        idx = 0
+        current_iteration = 0
+
+        while current_iteration < inserts_num:
+
+            current_iteration += 1
+
+            fst_idx = idx
+            while fst_idx < current_iteration * inserts_num:
+                self.db.join_table_first.insert_one({'id': fst_idx, 'a': f'a_value_{fst_idx}'})
+                fst_idx += 1
+            snd_idx = idx
+            while snd_idx < current_iteration * inserts_num:
+                self.db.join_table_second.insert_one({'id': snd_idx, 'b': f'b_value_{snd_idx}'})
+                snd_idx += 1
+            idx += current_iteration * inserts_num
+
+            start = time.time()
+            self.db.join_table_first.aggregate([{
+                '$lookup': {
+                    'from': 'join_table_second',
+                    'localField': 'id',
+                    'foreignField': 'id',
+                    'as': 'joined'
+                }
+            }])
+            end = time.time()
+            self.db.join_table.insert_one({'id': current_iteration, 'time': end - start})
 
         print('test finished')
 
